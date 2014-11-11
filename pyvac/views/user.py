57436ddd2717b21e106af24e8e37a9545e1e7f9c
sayoun <dejan.filipovic@gmail.com>
2014-11-11 22:52:06 +0100

    Add code for who's who view

--- pyvac/views/user.py
+++ pyvac/views/user.py
@@ -4,11 +4,17 @@ Pyvac User Management Views.
 
 Used by the connected user to edit its account.
 """
+import base64
+
+from pyramid.settings import asbool
+
 from pyvac.models import User
 from pyvac.helpers.i18n import trans as _
+from pyvac.helpers.ldap import LdapCache
+from pyvac.helpers.util import extract_cn
 
 from .account import AccountMixin
-from .base import EditView
+from .base import EditView, View
 
 
 class UserMixin(AccountMixin):
@@ -45,3 +51,40 @@ class ChangePassword(UserMixin, EditView):
             errors.append(_(u'passwords do not match'))
 
         return len(errors) == 0
+
+
+class Whoswho(View):
+
+    def render(self):
+
+        settings = self.request.registry.settings
+        use_ldap = False
+        if 'pyvac.use_ldap' in settings:
+            use_ldap = asbool(settings.get('pyvac.use_ldap'))
+
+        users = User.find(self.session)
+        ldap_users = {}
+        if use_ldap:
+            # synchronise user groups/roles
+            User.sync_ldap_info(self.session)
+            ldap = LdapCache()
+            ldap_users = ldap.list_users()
+
+            for user in users:
+                ldap_user = ldap_users.get(user.dn, {})
+                user.nickname = ldap_user.get('uid', ['-'])[0]
+                user.mobile = ldap_user.get('mobile', ['-'])[0]
+                user.unit = extract_cn(ldap_user.get('ou', ['-'])[0])
+                if user.manager_dn:
+                    user.name_manager = extract_cn(user.manager_dn)
+                else:
+                    user.name_manager = '-' or user.manager.name
+                jpegPhoto = ldap_user.get('jpegPhoto')
+                photo = None
+                if jpegPhoto:
+                    photo = base64.b64encode(jpegPhoto[0])
+                user.photo = photo
+
+        return {u'user_count': User.find(self.session, count=True),
+                u'users': users,
+                }
